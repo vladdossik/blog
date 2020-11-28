9af16cf141f8a3abe887a63824240ac18f5ecb7a
Vladdosiik <vladdosiik7540@gmail.com>
2020-11-28 20:09:53 +0300

    home page

--- src/main/java/com/vladdosiik/blog/controllers/MainController.java
+++ src/main/java/com/vladdosiik/blog/controllers/MainController.java
@@ -1,16 +1,16 @@
 package com.vladdosiik.blog.controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/")
+    public String home( Model model) {
+        model.addAttribute("title", "main page");
+        return "home";
     }
 
 }
