5a67c304a9d02e6d0547b0972f2b784201fc652c
Vladdosiik <vladdosiik7540@gmail.com>
2020-11-28 19:49:35 +0300

    start

---
+++ src/test/java/com/vladdosiik/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladdosiik.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
