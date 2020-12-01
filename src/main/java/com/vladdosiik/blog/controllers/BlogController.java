365fa23de994af16a3445f2c8fe7f77e3f754220
Vladdosiik <vladdosiik7540@gmail.com>
2020-12-02 00:24:53 +0300

    add articles to the blog

--- src/main/java/com/vladdosiik/blog/controllers/BlogController.java
+++ src/main/java/com/vladdosiik/blog/controllers/BlogController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -20,7 +23,12 @@ public class BlogController {
     }
     @GetMapping("/blog/add")
     public String blogAdd(Model model){
-
         return "blog_add";
+    }
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,@RequestParam String anons,@RequestParam String full_text, Model model){
+        Post post=new Post(title,anons,full_text);
+postRepository.save(post);
+        return "redirect:/blog";
     }
 }
--- src/main/java/com/vladdosiik/blog/models/Post.java
+++ src/main/java/com/vladdosiik/blog/models/Post.java
@@ -21,6 +21,7 @@ public class Post {
         this.id = id;
     }
 
+
     public String getTitle() {
         return title;
     }
@@ -52,4 +53,14 @@ public class Post {
     public void setViews(int views) {
         this.views = views;
     }
+    public Post(){
+
+    }
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+
+    }
+
 }
